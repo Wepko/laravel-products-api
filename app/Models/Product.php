69352dcd821c684e838b326462489c3630939f8e
Sotnikov Maksim <sotnikov@mateline.ru>
2026-01-19 12:46:41 +0400

    [migrations] add: system migration create-tables and update-tables, more base model

---
+++ app/Models/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends AbstractBaseModel
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'category_id',
+        'in_stock',
+        'rating',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'in_stock' => 'boolean',
+        'rating' => 'float',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
