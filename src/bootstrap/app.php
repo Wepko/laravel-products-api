ca6f64e305f7b2687f214147e82046090d3c872a
Sotnikov Maksim <sotnikov@mateline.ru>
2026-02-04 03:46:36 +0400

    [modify] add: new exception

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(using: function (Exceptions $exceptions): void {
+            $exceptions->render(using: function (Throwable $e, Request $request) {
+
+                // Для API-маршрутов возвращаем JSON
+                if (str_starts_with($request->path(), 'api/')) {
+                    return response()->json([
+                        'message' => $e->getMessage(),
+                        'error' => class_basename($e),
+                    ], method_exists($e, 'getStatusCode')
+                        ? $e->getStatusCode()
+                        : 500);
+                }
+            });
     })->create();
