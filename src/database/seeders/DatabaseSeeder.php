888f2435774fb803287f3fa4de7aebabf1bebe58
Sotnikov Maksim <sotnikov@mateline.ru>
2026-02-15 22:28:41 +0400

    [Modify] fix: dockerfile mysql_dto module

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,43 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Создаем тестового пользователя
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
 
-        // Создаем категории
-        $categories = Category::factory(10)->create();
+        $this->call([
+            TestSeeder::class,
+        ]);
 
-        // Настройки для пакетной вставки
-        $chunkSize = 1000; // Размер пачки для вставки
-        $totalProducts = 1000000; // Общее количество товаров
-        $productsPerCategory = 100000; // По 100,000 товаров на категорию
-
-        foreach ($categories as $category) {
-            for ($i = 0; $i < $productsPerCategory / $chunkSize; $i++) {
-                $products = [];
-
-                for ($j = 0; $j < $chunkSize; $j++) {
-                    $products[] = [
-                        'category_id' => $category->id,
-                        'name' => fake()->words(3, true),
-                        'price' => fake()->randomFloat(2, 1, 10000),
-                        'in_stock' => mt_rand(1, 100) <= 80 ? 1 : 0,
-                        'rating' => mt_rand(0, 50) / 10,
-                        'created_at' => now(),
-                        'updated_at' => now(),
-                    ];
-                }
-
-                Product::insert($products);
-
-                $currentCount = ($i + 1) * $chunkSize;
-                echo "Создано товаров для категории {$category->id}: {$currentCount}/{$productsPerCategory}\n";
-            }
-        }
-
-        echo "Всего создано товаров: " . Product::count() . "\n";
     }
 }
